85dba860ee54d6d3c2f2b9b7fee925b32e3ff265
ayush-sr02 <79581632+ayush-sr02@users.noreply.github.com>
2022-09-22 01:57:26 +0530

    Time: 85 ms (18.83%), Space: 19.1 MB (9.67%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
+    vector<pair<int,int>> movement = {{0,1},{0,-1},{1,0},{-1,0}};
     
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>&vis,int i,int j){
-        int n=grid.size(),m=grid[0].size();
-        if(i<0 or j<0 or i>=n or j>=m or vis[i][j]) return;
-        if(grid[i][j]=='0') return;
+    bool valid(int x,int y,int n,int m){
+        return x>=0 and y>=0 and x<n and y<m;
+    }
+    
+    void bfs(vector<vector<char>>& grid,int i,int j,vector<vector<int>> &vis,int n,int m){
+        queue<pair<int,int>> q;
+        q.push(make_pair(i,j));
         vis[i][j]=1;
-        dfs(grid,vis,i+1,j);
-        dfs(grid,vis,i-1,j);
-        dfs(grid,vis,i,j+1);
-        dfs(grid,vis,i,j-1);
+        while(!q.empty()){
+            auto node = q.front();
+            int x=node.first,y=node.second;
+            q.pop();
+            for(auto i:movement){
+                int xd = x+i.first;
+                int yd = y+i.second;
+                if(valid(xd,yd,n,m)){
+                    if(!vis[xd][yd] and grid[xd][yd]=='1'){
+                        q.push(make_pair(xd,yd));
+                        vis[xd][yd]=1;
+                    }
+                }
+                
+            }
+        }
     }
     
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size(),m=grid[0].size();
         vector<vector<int>> vis(n,vector<int> (m,0));
-        int ans=0;
+        int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] and grid[i][j]=='1'){
-                    dfs(grid,vis,i,j);
-                    ans++;
+                if(grid[i][j]=='1' and !vis[i][j]){
+                    bfs(grid,i,j,vis,n,m);
+                    cnt++;
                 }
             }
         }
-        return ans;
+        return cnt;
     }
 };
