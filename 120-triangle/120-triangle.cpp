576953d198387f37e623058484cacbab650bd52e
ayush-sr02 <79581632+ayush-sr02@users.noreply.github.com>
2022-09-29 19:25:35 +0530

    Time: 3 ms (98.73%), Space: 9.1 MB (10.30%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&triangle,int i,int j,int n,vector<vector<int>>&dp){
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = triangle[i][j]+solve(triangle,i+1,j,n,dp);
-        int right = triangle[i][j]+solve(triangle,i+1,j+1,n,dp);
-        return dp[i][j] = min(down,right);
+//     int solve(vector<vector<int>>&triangle,int i,int j,int n,vector<vector<int>>&dp){
+//         if(i==n-1) return triangle[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int down = triangle[i][j]+solve(triangle,i+1,j,n,dp);
+//         int right = triangle[i][j]+solve(triangle,i+1,j+1,n,dp);
+//         return dp[i][j] = min(down,right);
         
-    }
+//     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>> dp(n,vector<int> (n,-1));
-        return solve(triangle,0,0,n,dp);
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle[i][j]+dp[i+1][j];
+                int right = triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,right);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
