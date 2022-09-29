15b1977f832933a613484953cf8f8425384bf77e
ayush-sr02 <79581632+ayush-sr02@users.noreply.github.com>
2022-09-29 19:47:10 +0530

    Time: 656 ms (5.01%), Space: 46.7 MB (5.00%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     
-    int solve(int i,int j,int n,vector<vector<int>>&dp,vector<vector<int>>&matrix){
-        if(j<0 || j>=n) return 1e9;
+    int solve(vector<vector<int>>&matrix,int i,int j,vector<vector<int>>&dp){
         if(i==0) return matrix[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
-        int same = matrix[i][j]+solve(i-1,j,n,dp,matrix);
-        int left = matrix[i][j]+solve(i-1,j-1,n,dp,matrix);
-        int right = matrix[i][j]+solve(i-1,j+1,n,dp,matrix);
-        return dp[i][j] = min(left,min(right,same));
+        int up = matrix[i][j]+solve(matrix,i-1,j,dp);
+        int left=1e9;
+        if(j>0) left = matrix[i][j]+solve(matrix,i-1,j-1,dp);
+        int right=1e9;
+        if(j<matrix.size()-1) right = matrix[i][j]+solve(matrix,i-1,j+1,dp);
+        return dp[i][j] = min(up,min(left,right));
     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            ans=min(ans,solve(n-1,i,n,dp,matrix));
+        int ans=1e9;
+        for(int j=0;j<n;j++){
+            vector<vector<int>> dp(n,vector<int> (n,-1));
+            int x=solve(matrix,n-1,j,dp);
+            ans=min(x,ans);
         }
         return ans;
     }
