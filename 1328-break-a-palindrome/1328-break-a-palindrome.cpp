a3a4141f383438c2e8a6d9e73ed65a999f06c99f
ayush-sr02 <79581632+ayush-sr02@users.noreply.github.com>
2022-10-10 14:43:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string breakPalindrome(string palin) {
+        if(palin.size()==1){
+            return "";
+        }
+        int i;
+        for(i=0;i<palin.size();i++){
+            if(palin[i]!='a' && i!=palin.size()/2) {
+                palin[i] = 'a';
+                break;
+            }
+        }
+        if(i==palin.size()) palin[i-1]='b';
+        return palin;
+    }
+};
