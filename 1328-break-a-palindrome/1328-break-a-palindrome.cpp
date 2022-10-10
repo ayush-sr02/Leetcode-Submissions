738ac17a9c3d2c460dcdf6ccc4522303e613d1f2
ayush-sr02 <79581632+ayush-sr02@users.noreply.github.com>
2022-10-10 14:47:20 +0530

    Time: 2 ms (51.67%), Space: 6.2 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -8,7 +8,7 @@ public:
         for(i=0;i<palin.size();i++){
             if(palin[i]!='a' && i!=palin.size()/2) {
                 palin[i] = 'a';
-                break;
+                return palin;
             }
         }
         if(i==palin.size()) palin[i-1]='b';
