b1921760d49f69a59ede02c4922e365f44316b54
Ayush <ayushsh2002@gmail.com>
2022-12-02 18:01:22 +0530

    Time: 365 ms (17.07%), Space: 20.8 MB (32.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    
+    //create a frequency array for both the strings and sort them. If they are equal then we can attain one from the other
     
     bool closeStrings(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        sort(begin(s),end(s));
-        sort(begin(t),end(t));
-        if(s==t) return true;
         map<char,int> m1,m2;
         for(int i=0;i<s.size();i++) m1[s[i]]++;
-        for(int i=0;i<s.size();i++) m2[t[i]]++;
+        for(int i=0;i<t.size();i++) m2[t[i]]++;
         vector<int> v1,v2;
         for(auto i:m1){
-            if(m2.find(i.first)==m2.end()) return false;
+            if(m2.find(i.first)==m2.end()) return false;       // condition for unmatched character.
             v1.push_back(i.second);
         } 
         for(auto i:m2){
