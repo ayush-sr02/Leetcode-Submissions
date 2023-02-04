a402bf161d9a4c6571e1b937860fe36ccd4dfb2a
Ayush <ayushsh2002@gmail.com>
2023-02-05 02:04:52 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -11,25 +11,25 @@ class Solution
 	{
 	    // code here
 	    vector<int> indegree(V,0);
-	    queue<int> q;
-	    vector<int> ans;
 	    for(int i=0;i<V;i++){
 	        for(auto it:adj[i]){
 	            indegree[it]++;
 	        }
 	    }
+	    queue<int> q;
+	    vector<int> ans;
+	    
 	    for(int i=0;i<V;i++){
 	        if(indegree[i]==0) q.push(i);
 	    }
+	    
 	    while(!q.empty()){
-	        int node = q.front();
+	        int top = q.front();
 	        q.pop();
-	        ans.push_back(node);
-	        for(auto it:adj[node]){
+	        ans.push_back(top);
+	        for(auto it:adj[top]){
 	            indegree[it]--;
-	            if(indegree[it]==0){
-	                q.push(it);
-	            }
+	            if(indegree[it]==0) q.push(it);
 	        }
 	    }
 	    
