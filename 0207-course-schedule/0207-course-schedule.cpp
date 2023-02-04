811bbd8cd95a7d365b885883624580d6d34f84ba
Ayush <ayushsh2002@gmail.com>
2023-02-05 02:12:28 +0530

    Time: 18 ms (94.15%), Space: 13.9 MB (59.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool canFinish(int num, vector<vector<int>>& pre) {
-        int n=pre.size();
-        vector<int> adj[num];
+    bool canFinish(int num, vector<vector<int>>& p) {
         vector<int> indegree(num,0);
-        for(int i=0;i<n;i++){
-            adj[pre[i][0]].push_back(pre[i][1]);
-            indegree[pre[i][1]]++;
+        vector<int> adj[num];
+
+        for(auto i:p){
+            adj[i[0]].push_back(i[1]);
+            indegree[i[1]]++;
         }
         queue<int> q;
-        for(int i=0;i<num;i++)
-            if(indegree[i]==0)
-                q.push(i);
-        // vector<int> topo;
-        int cnt=0;
+        vector<int> topo;
+        
+        for(int i=0;i<num;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
         while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            // topo.push_back(node);
-            cnt++;
-            for(auto it:adj[node]){
+            int top = q.front(); q.pop();
+            topo.push_back(top);
+            for(auto it:adj[top]){
                 indegree[it]--;
-                if(indegree[it]==0)
-                    q.push(it);
+                if(indegree[it]==0) q.push(it);
             }
         }
-        // return topo.size()==num;
-        return cnt==num;
+        return topo.size()==num;
     }
 };
