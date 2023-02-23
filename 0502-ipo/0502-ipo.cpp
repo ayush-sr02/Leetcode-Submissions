08b7a4144c739757b4a2cd49fc619143f6c7ea9d
Ayush <ayushsh2002@gmail.com>
2023-02-23 11:15:38 +0530

    Time: 231 ms (69.78%), Space: 82.1 MB (53.52%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        vector<pair<int,int>>v;
-        for(int i=0;i<profits.size();i++){
-            v.push_back({capital[i],profits[i]});
+
+/* create list of pair and sort it according to increasing capital, then while current capital    is greater than that of the ith project push it's profit in the max heap. Then pick the    project with max profit and add it to your capital. */
+    
+    int findMaximizedCapital(int k, int w, vector<int>& p, vector<int>& c) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<p.size();i++){
+            v.push_back({c[i],p[i]});
         }
         sort(begin(v),end(v));
-        // for(auto i:v){
-        //     cout<<i.first<<" "<<i.second<<endl;
-        // }
-        priority_queue<int>pq;
+        priority_queue<int> pq;
         int i=0;
         while(k--){
             while(i<v.size() and w>=v[i].first){
-               pq.push(v[i].second);
-               i++; 
+                pq.push(v[i].second);
+                i++;
             }
-            if(pq.empty())break;
+            if(pq.empty()) continue;
             w+=pq.top();
             pq.pop();
         }
-       return w;
+        return w;
     }
-};  
+};
