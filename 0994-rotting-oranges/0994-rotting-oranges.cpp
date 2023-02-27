8335e0d461409e59f7b0183b2ea965dd02d219d6
Ayush <ayushsh2002@gmail.com>
2023-02-27 10:00:52 +0530

    Time: 9 ms (51.34%), Space: 14.3 MB (6.23%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    
-    vector<pair<int,int>> movement = {{-1,0},{1,0},{0,1},{0,-1}};
+    vector<vector<int>> movement = {{0,1},{0,-1},{1,0},{-1,0}};   
     
     bool valid(int i,int j,int n,int m){
         return i>=0 and j>=0 and i<n and j<m;
     }
     
     int orangesRotting(vector<vector<int>>& grid) {
+        int ans=0;
         int n=grid.size(),m=grid[0].size();
+        // coordinates,time
         queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2) q.push({{i,j},0});
             }
         }
-        int ans=0;
+        
         while(!q.empty()){
             int x=q.front().first.first;
             int y=q.front().first.second;
-            int temp = q.front().second;
+            int time=q.front().second;
+            ans=max(ans,time);
             q.pop();
-            ans=max(ans,temp);
-            for(auto it:movement){
-                int xd = x+it.first;
-                int yd = y+it.second;
+            for(auto i:movement){
+                int xd=x+i[0];
+                int yd=y+i[1];
                 if(valid(xd,yd,n,m) and grid[xd][yd]==1){
-                    q.push({{xd,yd},temp+1});
+                    q.push({{xd,yd},time+1});
                     grid[xd][yd]=2;
                 }
             }
         }
-        
-        for(auto i:grid){
-            for(auto j:i){
-                if(j==1) return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) return -1;
             }
         }
-        
         return ans;
     }
 };
