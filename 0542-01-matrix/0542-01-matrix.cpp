5b27cd833d314be5c40ce6f8b7e53d64fe1140fa
Ayush <ayushsh2002@gmail.com>
2023-02-27 10:14:17 +0530

    Time: 279 ms (6.43%), Space: 71.9 MB (5.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     
-    vector<pair<int,int>> movement = {{-1,0},{1,0},{0,-1},{0,1}};
+    vector<vector<int>> movement = {{0,1},{0,-1},{1,0},{-1,0}};
     
-    bool valid(int i,int j,int n,int m){
+    bool isValid(int i,int j,int n,int m){
         return i>=0 and j>=0 and i<n and j<m;
     }
     
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>, int>> q;
+        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     q.push({{i,j},0});
-                    vis[i][j]=1;
                 }
             }
         }
+        vector<vector<int>> ans(n,vector<int> (m,0));
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int step = q.front().second;
+            int x=q.front().first.first;
+            int y=q.front().first.second;
+            int time=q.front().second;
             q.pop();
-            dis[row][col]=step;
-            for(auto it:movement){
-                int xd = row+it.first;
-                int yd = col+it.second;
-                if(valid(xd,yd,n,m) and !vis[xd][yd]){
-                    q.push({{xd,yd},step+1});
-                    vis[xd][yd]=1;
+            for(auto i:movement){
+                int xd=x+i[0];
+                int yd=y+i[1];
+                if(isValid(xd,yd,n,m) and mat[xd][yd]==1){
+                    q.push({{xd,yd},time+1});
+                    mat[xd][yd]=0;
+                    ans[xd][yd]=time+1;
                 }
             }
         }
-        return dis;
+        return ans;
     }
 };
