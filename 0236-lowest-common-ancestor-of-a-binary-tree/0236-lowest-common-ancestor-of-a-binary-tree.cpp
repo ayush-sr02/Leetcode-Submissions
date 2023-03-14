96ac61fdb0e9b0ae73acbd1af4eda9b73a97e44e
Ayush <ayushsh2002@gmail.com>
2023-03-15 00:53:07 +0530

    Time: 16 ms (76.06%), Space: 14.3 MB (63.32%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,28 +9,12 @@
  */
 class Solution {
 public:
-    
-    bool find(vector<int> &v,TreeNode* p,TreeNode* root){
-        if(root==NULL) return false;
-        v.push_back(root->val);
-        if(root==p) return true;
-        if(find(v,p,root->left) or find(v,p,root->right)) return true;
-        v.pop_back();
-        return false;
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<int> v1,v2;
-        find(v1,p,root);
-        find(v2,q,root);
-        TreeNode* ans=new TreeNode(-1);
-        int i=0,j=0;
-        while(i<v1.size() and j<v2.size()){
-            if(v1[i]==v2[j]){
-                ans->val=v1[i];
-                i++;j++;
-            }else break;
-        }
-        return ans;
+        if(root==NULL or root==p or root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left==nullptr) return right;
+        if(right==nullptr) return left;
+        return root;
     }
 };
