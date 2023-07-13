0604c8df6e500f12a34904f855dfd3c90105aee6
Ayush <ayushsh2002@gmail.com>
2023-07-13 14:40:43 +0530

    Time: 39 ms (20.47%), Space: 14.4 MB (45.21%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<vector<vector<int>>> dp;
     
-    int solve(int i,int buy,int k,vector<int>&prices){
-        if(i>=prices.size() or k<0) return 0;
-        if(dp[i][buy][k]!=-1) return dp[i][buy][k];
+    int solve(int i,int buy,int t,vector<int>&prices){
+        if(i==prices.size() or t<0) return 0;
+        if(dp[i][buy][t]!=-1) return dp[i][buy][t];
         if(!buy){
-            return dp[i][buy][k] = max(-prices[i]+solve(i+1,1,k-1,prices),solve(i+1,buy,k,prices));
+            return dp[i][buy][t] =  max(-prices[i]+solve(i+1,!buy,t-1,prices),solve(i+1,buy,t,prices));
         }else{
-            return dp[i][buy][k] = max(prices[i]+solve(i+1,0,k,prices),solve(i+1,buy,k,prices));
+            return dp[i][buy][t] = max(prices[i]+solve(i+1,!buy,t,prices),solve(i+1,buy,t,prices));
         }
     }
     
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        dp.resize(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        dp.resize(n,vector<vector<int>> (2,vector<int> (k+1,-1)));
         return solve(0,0,k,prices);
     }
 };
