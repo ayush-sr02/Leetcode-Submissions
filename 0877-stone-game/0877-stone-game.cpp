1c8cba99e47db7c3b4bef239c2225be1996acd69
Ayush <ayushsh2002@gmail.com>
2023-08-10 01:13:44 +0530

    Time: 4 ms (69.72%), Space: 7.5 MB (79.01%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<int>&piles,int i,int j,int alice){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(alice) return dp[i][j]=max(piles[i]+solve(piles,i+1,j,!alice),piles[j]+solve(piles,i,j-1,!alice));
-        return dp[i][j]=min(-piles[i]+solve(piles,i+1,j,!alice),-piles[j]+solve(piles,i,j-1,!alice));
-    }
-    
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        dp.resize(n,vector<int> (n,-1));
-        return solve(piles,0,n-1,1)>0;
+        return true;
     }
 };
