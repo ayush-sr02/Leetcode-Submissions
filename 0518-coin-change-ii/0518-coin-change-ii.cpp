768afb6a7fffe119c366c50194fa73ef636ccd7c
Ayush <ayushsh2002@gmail.com>
2023-08-11 07:00:27 +0530

    Time: 37 ms (44.80%), Space: 18.7 MB (24.11%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    
+    int solve(int i,int target,vector<int>&coins){
+        if(target==0) return 1;
+        if(i>=coins.size()) return 0;
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notPick = solve(i+1,target,coins);
+        int pick=0;
+        if(target>=coins[i]) pick = max(solve(i+1,target-coins[i],coins),solve(i,target-coins[i],coins));
+        return dp[i][target] = pick+notPick;
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        sort(begin(coins),end(coins));
+        dp.resize(n,vector<int> (amount+1,-1));
+        return solve(0,amount,coins);
+    }
+};
