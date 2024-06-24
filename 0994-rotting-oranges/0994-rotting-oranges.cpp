b2432f46902617bd541066e6338b3e301bedfe70
Ayush <ayushsh2002@gmail.com>
2024-06-24 23:06:13 +0530

    Time: 3 ms (84.44%), Space: 15.8 MB (83.80%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> movement = {{0,1},{0,-1},{1,0},{-1,0}};   
     
-    bool valid(int i,int j,int n,int m){
-        return i>=0 and j>=0 and i<n and j<m;
+    bool isvalid(int i,int j, int n, int m){
+        return i>=0 and i<n and j>=0 and j<m;
     }
     
+    vector<pair<int,int>> v = {{-1,0},{1,0},{0,-1},{0,1}};
     int orangesRotting(vector<vector<int>>& grid) {
         int ans=0;
-        int n=grid.size(),m=grid[0].size();
-        // coordinates,time
-        queue<pair<pair<int,int>,int>> q;
+        int n=grid.size(), m=grid[0].size();
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2) q.push({{i,j},0});
+                if(grid[i][j]==2) q.push({i,j});
             }
         }
-        
         while(!q.empty()){
-            int x=q.front().first.first;
-            int y=q.front().first.second;
-            int time=q.front().second;
-            ans=max(ans,time);
-            q.pop();
-            for(auto i:movement){
-                int xd=x+i[0];
-                int yd=y+i[1];
-                if(valid(xd,yd,n,m) and grid[xd][yd]==1){
-                    q.push({{xd,yd},time+1});
-                    grid[xd][yd]=2;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                pair<int,int> p = q.front();
+                q.pop();
+                for(auto it:v){
+                    int dx = p.first+it.first;
+                    int dy = p.second+it.second;
+                    if(isvalid(dx,dy,n,m)){
+                        if(grid[dx][dy]==1){
+                            grid[dx][dy]=-1;
+                            q.push({dx,dy});
+                        }
+                    }
                 }
             }
+            ans++;
         }
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1) return -1;
             }
         }
-        return ans;
+        return ans==0?0:ans-1;
     }
 };
